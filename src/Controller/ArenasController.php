ae0e03ddab3b7404d2c57321b08faf9cc71f2112
qmatejka <qmister.k@gmail.com>
2016-09-26 12:32:27 +0200

    second project import

---
+++ src/Controller/ArenasController.php
@@ -0,0 +1,35 @@
+<?php
+namespace App\Controller;
+use App\Controller\AppController;
+/**
+* Personal Controller
+* User personal interface
+*
+*/
+class ArenasController  extends AppController
+{
+public function index()
+{
+	$this->set('myname', "Julien Falconnet");
+}
+
+public function diary()
+{
+	
+}
+
+public function login()
+{
+	
+}
+
+public function sight()
+{
+	
+}
+
+public function fighter()
+{
+	
+}
+}
---
+++ src/Template/Arenas/index.ctp
@@ -0,0 +1,1 @@
+Bienvenue <?php echo $myname;?> dans WebArena
